2cb11eebc99fe5eda5386ecf0458249f0a660be1
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-05-29 19:35:24 +0200

    Springboot initializer Gradle

---
+++ src/main/java/org/fugerit/java/fjdocquickstartdemo/springboot3/FjDocQuickstartDemoSpringboot3Application.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.fjdocquickstartdemo.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FjDocQuickstartDemoSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FjDocQuickstartDemoSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/fugerit/java/fjdocquickstartdemo/springboot3/FjDocQuickstartDemoSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.fjdocquickstartdemo.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FjDocQuickstartDemoSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
