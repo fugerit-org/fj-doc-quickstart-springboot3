ae0c69b7daa781f4b12c86b59a110f9991c79a01
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-05-30 10:00:03 +0200

    Added test case

--- src/test/java/org/fugerit/java/fjdocquickstartdemo/springboot3/FjDocQuickstartDemoSpringboot3ApplicationTests.java
+++ src/test/java/org/fugerit/java/fjdocquickstartdemo/springboot3/FjDocQuickstartDemoSpringboot3ApplicationTests.java
@@ -1,13 +1,30 @@
 package org.fugerit.java.fjdocquickstartdemo.springboot3;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import javax.ws.rs.core.MediaType;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+  classes = FjDocQuickstartDemoSpringboot3Application.class)
+@AutoConfigureMockMvc
 class FjDocQuickstartDemoSpringboot3ApplicationTests {
 
+	@Autowired
+    private MockMvc mvc;
+	
 	@Test
-	void contextLoads() {
+	void testPdf() throws Exception {
+		mvc.perform(get("http://localhost:8080/pdftest")
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isOk());
 	}
 
 }
